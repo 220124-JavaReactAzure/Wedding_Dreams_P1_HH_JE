04ac96a76893296a0b168a877f25f6db5f1942fb
Hannah Hospital <delopitalhannah@gmail.com>
2022-02-17 14:27:58 -0600

    Testing pushing to a new branch

---
+++ src/main/java/weddingDreams/models/Users.java
@@ -0,0 +1,20 @@
+package weddingDreams.models;
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name="users")
+public class Users {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="user_id", unique=true, nullable=false)
+
+}
